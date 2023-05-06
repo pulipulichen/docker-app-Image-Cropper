79c502737ce23a82672ae0e3a06f68e8fc110234
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-05-06 22:13:39 +0800

    Sat May  6 10:13:39 PM CST 2023

--- app/image-trim.js
+++ app/image-trim.js
@@ -30,7 +30,7 @@ let main = async function () {
       await ShellExec(`convert "${file}" -trim +repage "${path.resolve(dirname, filenameNoExt + '-cropped' +ext)}"`)
     }
     else {
-      await ShellExec(`convert "${file}" -alpha set -bordercolor white -border 1 -fill none -fuzz 3% -draw "color 0,0 floodfill" -shave 1x1 -trim +repage "${path.resolve(dirname, filenameNoExt + '-cropped' +ext)}"`)
+      await ShellExec(`convert "${file}" -alpha set -bordercolor white -border 1 -fill none -fuzz 20% -draw "color 0,0 floodfill" -shave 1x1 -trim +repage "${path.resolve(dirname, filenameNoExt + '-cropped' +ext)}"`)
     }
     // convert -gravity center "c.png" -flatten -fuzz 1% -trim +repage -resize 64x64 -extent 64x64 "b.ico"
   }
