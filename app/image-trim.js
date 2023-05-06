b06d533d13350de7391fbae4f3bc7f3eda15a9be
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-05-06 22:20:53 +0800

    Sat May  6 10:20:53 PM CST 2023

--- app/image-trim.js
+++ app/image-trim.js
@@ -23,14 +23,17 @@ let main = async function () {
     // await ShellExec(`convert "${file}" -trim +repage "${path.resolve(dirname, filenameNoExt + '-cropped.' +ext)}"`)
     // await ShellExec(`convert "${file}" -transparent white -trim +repage "${path.resolve(dirname, filenameNoExt + '-cropped.' +ext)}"`)
 
-    let channels = await ShellExec(`identify -format '%[channels]' "${file}"`)
+    // let channels = await ShellExec(`identify -format '%[channels]' "${file}"`)
+    let channels = await ShellExec(`convert "${file}" -channel a -separate -format "%[fx:mean]" info:`)
+
     // fs.writeFileSync(file + '-channels.txt', channels, 'utf8')
-    if (channels.indexOf('a') > -1) {
+    // if (channels.indexOf('a') > -1) {
+    if (channels !== '1') {
       // await ShellExec(`convert "${file}" -alpha set -bordercolor transparent -border 1 -fill none -fuzz 3% -draw "color 0,0 floodfill" -shave 1x1 -trim +repage "${path.resolve(dirname, filenameNoExt + '-cropped' +ext)}"`)
       await ShellExec(`convert "${file}" -trim +repage "${path.resolve(dirname, filenameNoExt + '-cropped' +ext)}"`)
     }
     else {
-      await ShellExec(`convert "${file}" -alpha set -bordercolor white -border 1 -fill none -fuzz 20% -draw "color 0,0 floodfill" -shave 1x1 -trim +repage "${path.resolve(dirname, filenameNoExt + '-cropped' +ext)}"`)
+      await ShellExec(`convert "${file}" -alpha set -bordercolor white -border 1 -fill none -fuzz 20% -draw "color 0,0 floodfill" -shave 1x1 -fuzz 20% -trim +repage "${path.resolve(dirname, filenameNoExt + '-cropped' +ext)}"`)
     }
     // convert -gravity center "c.png" -flatten -fuzz 1% -trim +repage -resize 64x64 -extent 64x64 "b.ico"
   }
