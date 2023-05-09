8b4142d31d98861e9bf12d643955a425ad413382
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-05-10 00:36:10 +0800

    Wed May 10 12:36:10 AM CST 2023

--- bin/image-trim.sh
+++ bin/image-trim.sh
@@ -103,16 +103,3 @@ else
     node "/tmp/${PROJECT_NAME}/index.js" "${var}"
   fi
 fi
-
-
-
-echo "Press any key to continue"
-while [ true ] ; do
-read -t 3 -n 1
-if [ $? = 0 ] ; then
-exit ;
-else
-echo "waiting for the keypress"
-fi
-done
-
