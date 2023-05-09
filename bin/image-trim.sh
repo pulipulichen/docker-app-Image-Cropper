b8b634113492edc3426c90528a60c1b614fb5e05
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-05-10 00:23:42 +0800

    Wed May 10 12:23:42 AM CST 2023

--- bin/image-trim.sh
+++ bin/image-trim.sh
@@ -98,13 +98,13 @@ fi
 
 
 
-# echo "Press any key to continue"
-# while [ true ] ; do
-# read -t 3 -n 1
-# if [ $? = 0 ] ; then
-# exit ;
-# else
-# echo "waiting for the keypress"
-# fi
-# done
+echo "Press any key to continue"
+while [ true ] ; do
+read -t 3 -n 1
+if [ $? = 0 ] ; then
+exit ;
+else
+echo "waiting for the keypress"
+fi
+done
 
